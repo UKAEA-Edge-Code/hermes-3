4d2c8713206bee32206670f64bcd39c0b29859ac
Mike Kryjak <mikekryjak@gmail.com>
2026-04-08 15:57:18 +0100

    Add vantage component unit test

---
+++ tests/unit/test_vantage.cxx
@@ -0,0 +1,44 @@
+#include <cmath>
+
+#include <bout/bout_types.hxx>
+#include <bout/region.hxx>
+
+#include "gtest/gtest.h"
+
+#include "fake_mesh_fixture.hxx"
+#include "../../bout-particle-push.hxx"
+#include "../../include/component.hxx"
+#include "fake_mesh_fixture.hxx"
+#include "test_extras.hxx" // FakeMesh
+
+/// Global mesh
+namespace bout {
+namespace globals {
+extern Mesh* mesh;
+} // namespace globals
+} // namespace bout
+
+// The unit tests use the global mesh
+using namespace bout::globals;
+
+using VantageTest = FakeMeshFixture;
+
+class VantageTest : public FakeMeshFixture {
+public:
+  VantageTest()
+      : FakeMeshFixture(), options({{"units",
+                                     {{"eV", 1.0},
+                                      {"meters", 1.0},
+                                      {"seconds", 1.0},
+                                      {"inv_meters_cubed", 1e19}}}}),
+        component("test", options, nullptr) {}
+  Options options;
+  Vantage component;
+};
+
+// Test that the component can be created
+TEST_F(VantageTest, CreateComponent) {
+  Options options;
+
+  Vantage const component("test", options, nullptr);
+}
