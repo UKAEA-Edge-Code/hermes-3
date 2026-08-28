f8f0f5fc69714c762caa8208fc41ffa1387370ee
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2026-08-28 16:36:35 +0100

    Refactor diagnostics calculation to enable the calculation of derived moments such as pressure and temperature.

--- src/vantage.cxx
+++ src/vantage.cxx
@@ -73,7 +73,6 @@ void write_kinetic_velocity_moment_diagnostics(
     std::shared_ptr<PetscInterface::DMPlexInterface>& neso_mesh,
     std::shared_ptr<PetscInterface::DMPlexProjectEvaluateDG>& project_eval_dg0,
     std::shared_ptr<ParticleGroup>& A_particle_group,
-    std::vector<double>& dof_kinetic_mesh_scalar,
     BoutReal N_w, BoutReal mass){
   // update the necessary particle properties for the moments
   // define the lambda updating the moments
@@ -98,46 +97,67 @@ void write_kinetic_velocity_moment_diagnostics(
   lambda_update_moment_kernels(static_particle_sub_group(A_particle_group));
   // write the data
   VTK::VTKHDF vtk_writer(vtkhdf_filename, neso_mesh->get_comm());
-  const std::size_t num_cells_owned_kinetic_mesh = dof_kinetic_mesh_scalar.size();
+  const std::size_t num_cells_owned_kinetic_mesh = static_cast<size_t>(neso_mesh->get_cell_count());
+  // vectors to hold the diagnosed moments
+  std::vector<REAL> density(num_cells_owned_kinetic_mesh);
+  std::vector<REAL> energy(num_cells_owned_kinetic_mesh);
+  std::vector<REAL> gamma(ndimv*num_cells_owned_kinetic_mesh);
+  std::vector<REAL> uvector(ndimv*num_cells_owned_kinetic_mesh);
+  std::vector<REAL> pressure(num_cells_owned_kinetic_mesh);
+  std::vector<REAL> temperature(num_cells_owned_kinetic_mesh);
   // mesh data only CellData not yet filled on each cell
   std::vector<VTK::UnstructuredCell> dvtk0 = neso_mesh->dmh->get_vtk_cell_data();
   std::vector<std::map<std::string, double>> cell_data(num_cells_owned_kinetic_mesh);
   // attempt to explore diagnostics on the DMPlex
   // extract density
-  // get whatever data is in particle weights
+  // get data from averages over the diagnostic particle weights
   project_eval_dg0->project(A_particle_group, Sym<REAL>("WEIGHT"));
   // project to the kinetic dof vector
-  project_eval_dg0->get_dofs(1, dof_kinetic_mesh_scalar);
-  for (size_t ic=0; ic < num_cells_owned_kinetic_mesh; ic++){
-    // multiply by any factors not handled in the project step
-    dof_kinetic_mesh_scalar.at(ic) *= N_w;
-    // insert a map entry at this ic
-    cell_data.at(ic).insert({"density", dof_kinetic_mesh_scalar.at(ic)});
-  }
+  project_eval_dg0->get_dofs(1, density);
   // energy
   project_eval_dg0->project(A_particle_group, Sym<REAL>("WEIGHT_V2"));
   // project to the kinetic dof vector
-  project_eval_dg0->get_dofs(1, dof_kinetic_mesh_scalar);
-  for (size_t ic=0; ic < num_cells_owned_kinetic_mesh; ic++){
-    // multiply by any factors not handled in the project step (weight factor * mass / 2)
-    dof_kinetic_mesh_scalar.at(ic) *= 0.5*N_w*mass;
-    // insert a map entry at this ic
-    cell_data.at(ic).insert({"energy", dof_kinetic_mesh_scalar.at(ic)});
-  }
+  project_eval_dg0->get_dofs(1, energy);
   // mean flow Gamma = nu
   project_eval_dg0->project(A_particle_group, Sym<REAL>("WEIGHT_V"));
   // project to the kinetic dof vector
-  // a dummy vector for ndimv component vector data
-  std::vector<REAL> dof_kinetic_mesh_vector(ndimv*num_cells_owned_kinetic_mesh);
-  project_eval_dg0->get_dofs(2, dof_kinetic_mesh_vector);
+  project_eval_dg0->get_dofs(2, gamma);
+  // scalar variables
+  for (size_t ic=0; ic < num_cells_owned_kinetic_mesh; ic++){
+    // multiply by any factors not handled in the project step
+    density.at(ic) *= N_w;
+    // (weight factor * mass / 2)
+    energy.at(ic) *= 0.5*N_w*mass;
+    // insert map entries at this ic
+    cell_data.at(ic).insert({"density", density.at(ic)});
+    cell_data.at(ic).insert({"energy", energy.at(ic)});
+  }
+  // vector variables
   for (size_t ic=0; ic < num_cells_owned_kinetic_mesh; ic++){
     for (size_t dim=0; dim < ndimv; dim++){
       const size_t jc = ic*ndimv + dim; // compound index covering all cells and dimensions
-      // multiply by any factors not handled in the project step (weight factor * mass / 2)
-      dof_kinetic_mesh_vector.at(jc) *= N_w;
+      // multiply by any factors not handled in the project step
+      gamma.at(jc) *= N_w;
+      // obtain the derived quantity uvector
+      uvector.at(jc) = gamma.at(jc) / density.at(ic);
       // insert a map entry at this ic
-      cell_data.at(ic).insert({fmt::format("gamma_{}",dim), dof_kinetic_mesh_vector.at(jc)});
+      cell_data.at(ic).insert({fmt::format("gamma_{}",dim), gamma.at(jc)});
+      cell_data.at(ic).insert({fmt::format("uvector_{}",dim), uvector.at(jc)});
     }
+  }
+  // derived scalar variables
+  for (size_t ic=0; ic < num_cells_owned_kinetic_mesh; ic++){
+    // calculate pressure = (2E - m n u^2 ) / ndimv
+    pressure.at(ic) = 2.0*energy.at(ic);
+    for (size_t dim=0; dim < ndimv; dim++){
+      const size_t jc = ic*ndimv + dim;
+      pressure.at(ic) -= mass*density.at(ic)*uvector.at(jc)*uvector.at(jc);
+    }
+    pressure.at(ic) /= ndimv;
+    temperature.at(ic) = pressure.at(ic)/density.at(ic);
+    // insert map entries at this ic
+    cell_data.at(ic).insert({"pressure", pressure.at(ic)});
+    cell_data.at(ic).insert({"temperature", temperature.at(ic)});
   }
   for (size_t ic=0; ic < static_cast<size_t>(num_cells_owned_kinetic_mesh); ic++){
     // fill the VTK::UnstructuredCell value appropriately
@@ -1315,7 +1335,7 @@ Vantage::Vantage(std::string name, Options& alloptions, Solver* UNUSED(solver))
     write_kinetic_velocity_moment_diagnostics(
       make_output_path("BOUT.dmp.vantage.particle.moments.vtkhdf", alloptions),
       neso_mesh, project_eval_dg0,
-      A_particle_group, dof_kinetic_mesh_scalar, N_w, AA);
+      A_particle_group, N_w, AA);
 
     // Calculate neutral density and sources for initial condition
     calculate_neutral_density_in_place(neutral_density,
